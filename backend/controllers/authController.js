97ea079576d880697ac1333c6e1f1a3728a61822
Anuranjan <itanuranjan@gmail.com>
2025-08-23 02:17:12 +0530

    same site none added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,7 +64,7 @@ const loginUser = async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 30 * 24 * 60 * 60 * 1000
       });
 
@@ -89,7 +89,7 @@ const logoutUser = (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
   });
   res.json({ message: 'Logged out successfully' });
 };
