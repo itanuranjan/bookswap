2fbe189e3ba73ef82d4b0608410267cef16490d7
Anuranjan <itanuranjan@gmail.com>
2025-08-23 01:29:02 +0530

    onrender added

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://bookswap-9y56.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
